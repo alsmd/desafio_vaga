6c104d22836dc2679225220d3c21b5fe2eca7f5a
Celebro01 <dangerveneta@gmail.com>
2021-04-01 13:13:51 -0300

    base do projeto

---
+++ projeto/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+}
+#app{
+  min-height: 100vh;
+  background-image: url('http://localhost:8080/assets/background.jpg');
+  background-position: bottom;
+}
+</style>
